0743aec96040eb29ddf11596718932f9ae774d96
Ianchoi4253 <wonyoungida4253@gmail.com>
2024-11-07 20:05:45 -0500

    Homework
    Just review and work.(11/7/24)

---
+++ week1/note.py
@@ -0,0 +1,31 @@
+# name = value
+x = 90
+character_e = "e"
+pi = 3.14
+# print(character_e)
+# print(pi)
+
+i = True
+l = None
+# print(i)
+# print(l)
+
+num = 7
+conv = float(num)
+# print(conv)
+a = 1
+b = 1
+# print( str(a) +str(b))
+# print("a"+"b")
+# print(a<=b)
+
+# print(a<b and a>=b)
+# print(a is not b)
+
+# user_input = input("please enter number for a: ")
+# user = input("please enter number for b:")
+# print ("result is,", (int(user_input) + int(user)))
+
+user_input = input("please enter number for a: ")
+user = input("please enter number for b:")
+print ("is a greater than b?", (user_input > user ))
