26d67692f19080cca840cfdeaa08c7f01a450643
Ianchoi4253 <wonyoungida4253@gmail.com>
2024-11-14 17:46:32 -0500

    Update note.py

--- week1/note.py
+++ week1/note.py
@@ -14,7 +14,7 @@ num = 7
 conv = float(num)
 # print(conv)
 a = 1
-b = 1
+b = 2
 # print( str(a) +str(b))
 # print("a"+"b")
 # print(a<=b)
@@ -26,6 +26,27 @@ b = 1
 # user = input("please enter number for b:")
 # print ("result is,", (int(user_input) + int(user)))
 
-user_input = input("please enter number for a: ")
-user = input("please enter number for b:")
-print ("is a greater than b?", (user_input > user ))
+# user_input = input("please enter number for a: ")
+# user = input("please enter number for b:")
+# print ("is a greater than b?", (user_input > user ))
+# p=55
+# hi="hi"
+# pi = 3.14
+# print(p)
+# print(hi)
+number=66
+hello=500
+# test=float(number)
+# # print(test)r)
+# test2=str(number)
+# print(test2)
+# print(number<hello and number>=hello)
+
+# testing= input("please enter number for a:")
+# tester= input("please enter a number for b:")
+# print("is a greater than b?",(testing>tester))
+project = input("please enter a number for a:")
+projecting= input("please enter a number for b:")
+creater = input("please enter  number for c:")
+creating = input("please enter a number for d:")
+print("is a times b greater than c times d",(int(project)*int(projecting) >int(creater)*int(creating)))
