c78cc8d499c74059d2dcf2a3d676c932a8688eaa
Ianchoi4253 <wonyoungida4253@gmail.com>
2024-12-04 20:18:32 -0500

    12/4/24

--- Helloworld.py
+++ Helloworld.py
@@ -1,13 +1,10 @@
-<<<<<<< Updated upstream
 print("What is 7*5")
 print("It is", 7*5)
 print("Hi nice to meet you")
 print("Sorry")
-=======
 print("Hello")
 #The meaning is "Hi"
 print("")
 #My name
 print("I am 30 years old")
 #Mistake of age
->>>>>>> Stashed changes
---
+++ week4/note.py
@@ -0,0 +1,33 @@
+# # 1. assign [Fortnite,Elden Ring,Call of Duty] to a variable called "games".
+# games_str = "fortnite,elden ring,call of duty"
+# games_list = games_str.split(",")
+# print(games_list)
+# games_list.insert(2,"LOL")
+# print(games_list)
+
+# if boolean(condition):
+#   statement
+
+# num = int(input("Please enter the number: "))
+# print(num % 2 == 1)
+# x = int(input("Please enter the number: "))
+#
+# if x % 2 != 0 or x % 5 !=0:
+#     print("x is not good number")
+# # elif x % 5 != 0:
+# #     print("x is not good number")
+# else:
+#     print("x is a good number")
+
+x = 10
+y = 30
+
+if x > 5:
+    if y < 30:
+        print("x is greater than 5 and y is less than 30")
+    elif x > 15:
+        print("x is greater than 15 and y is greater than 30")
+    else:
+        print("x is greater than 5 but y is not less than 30")
+else:
+    print("x is not greater than 5")
