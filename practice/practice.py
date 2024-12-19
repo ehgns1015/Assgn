55cc7eb548c99196529bae53702540765dd6da5a
Ianchoi4253 <wonyoungida4253@gmail.com>
2024-12-18 20:19:29 -0500

    12/18/24

--- practice/practice.py
+++ practice/practice.py
@@ -4,17 +4,17 @@
 # Problem: Assign two numbers to variables and print their sum.
 num1 = 5
 num2 = 10
-# Write the code here: Print the sum of num1 and num2.
-
+# Write the code here:
+print (num1 + num2)
 
 # Write the code here: Print the difference between num1 and num2.
-
+print(num1 - num2)
 # Write the code here: Print the product of num1 and num2.
-
+print(num1*num2)
 # Write the code here: Print the quotient of num2 divided by num1.
-
+print(num1/num2)
 # Write the code here: Print the remainder of num2 divided by num1.
-
+print(num1 % num2)
 # ===========================
 # 2. Datatypes Practice
 # ===========================
@@ -25,17 +25,25 @@ var_str = "Hello, Ian!"
 var_bool = True
 
 # Write the code here: Print the type of each variable.
-
+print(type(var_int))
+print(type(var_float))
+print(type(var_bool))
+print(type(var_str))
 # Write the code here: Print "Hello, Ian! Are you 13 years old?" without using constants
-
+yrs = 13
+print("Hello, Ian! Are you", yrs, "years old?")
+# num_of_kill = 0
+# num_of_kill = num_of_kill + 1
+# print(1)
+# print(num_of_kill)
 # ===========================
 # 3. Type Casting Practice
 # ===========================
 # Problem: Take two numbers as input, cast them to integers, and add them together.
 num_str1 = input("Enter the first number: ")
 num_str2 = input("Enter the second number: ")
-# Write the code here: Cast num_str1 and num_str2 to integers and print their sum.
-
+# Write the code here: Cast num_str1 and num_str2 to integers and print their sum.1
+print (int(num_str1) + int(num_str2))
 # ===========================
 # 4. Comparison Operators Practice
 # ===========================
@@ -46,7 +54,12 @@ num2 = int(input("Enter the second number: "))
 # "num1 is greater" if num1 is greater,
 # "num2 is greater" if num2 is greater,
 # "Both numbers are equal" if they are equal.
-
+if num1 > num2:
+    print("num1 is greater")
+elif num1 <= num2:
+    print("num2 is greater")
+else:
+    print("x is greater than 5 but y is not less than 30")
 # ===========================
 # 5. Logical Operators Practice
 # ===========================
@@ -54,6 +67,18 @@ num2 = int(input("Enter the second number: "))
 num1 = int(input("Enter the first number: "))
 num2 = int(input("Enter the second number: "))
 # Write the code here: Check if both num1 and num2 are even using a logical operator and print the message if both are even.
+# num1 and num2 are bot even
+# num1 and num2 are both odd
+# num1 is odd
+# num2 is odd
+if num1 % 2 ==0  and num2 % 2 == 0:
+    print("both are even")
+elif num1 % 2 !=0 and num2 % 2 !=0:
+    print("both are odd")
+elif num1 % 2 != 0:
+    print("num 1 is odd")
+else :
+    print("num2 is odd")
 
 # ===========================
 # 6. List Practice
@@ -66,10 +91,14 @@ for i in range(5):
     num_list.append(num)
 
 # Write the code here: Print all numbers stored in the list.
-
+print(num_list)
 # ===========================
 # 7. Conditionals Practice
 # ===========================
 # Problem: Check if a given number is odd or even.
 num = int(input("Enter a number: "))
-# Write the code here: Print "Odd" if the number is odd, and "Even" if the number is even.
+# # Write the code here: Print "Odd" if the number is odd, and "Even" if the number is even.
+if num % 2==0:
+    (print("x is a even number"))
+else:
+    print("x is a odd number")
--- week4/note.py
+++ week4/note.py
@@ -12,22 +12,28 @@
 # print(num % 2 == 1)
 # x = int(input("Please enter the number: "))
 #
+# x=10
+# y =30
 # if x % 2 != 0 or x % 5 !=0:
 #     print("x is not good number")
 # # elif x % 5 != 0:
 # #     print("x is not good number")
 # else:
 #     print("x is a good number")
+#
+# x = 10
+# y = 30
 
-x = 10
-y = 30
+# if x > 5:
+#     if y < 30:
+#         print("x is greater than 5 and y is less than 30")
+#     elif x > 15:
+#         print("x is greater than 15 and y is greater than 30")
+#     else:
+#         print("x is greater than 5 but y is not less than 30")
+# else:
+#     print("x is not greater than 5")
 
-if x > 5:
-    if y < 30:
-        print("x is greater than 5 and y is less than 30")
-    elif x > 15:
-        print("x is greater than 15 and y is greater than 30")
-    else:
-        print("x is greater than 5 but y is not less than 30")
-else:
-    print("x is not greater than 5")
+x = int(input("Enter a number: "))
+result = "x is even" if x % 2 ==0 else "x is odd"
+print(result)
