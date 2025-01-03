86e18bb12e115718e3db8ee96655c2e303017d4c
ehgns1015 <ehgns1015@gmail.com>
2025-01-03 09:31:12 +0900

    loop

--- practice/practice_2.py
+++ practice/practice_2.py
@@ -5,10 +5,37 @@ odd_numbers = []
 even_numbers = []
 
 # Continue the same way for the rest of the list items
-
+# let x is any number
+# x % 2 = 0, that means even
+# x % 2 = 1, that means odd
+if numbers[0] % 2 == 0:
+    even_numbers.append(numbers[0])
+else:
+    odd_numbers.append(numbers[0])
+if numbers[1] % 2 == 0:
+    even_numbers.append(numbers[1])
+else:
+    odd_numbers.append(numbers[1])
+if numbers[2] % 2 == 0:
+    even_numbers.append(numbers[2])
+else:
+    odd_numbers.append(numbers[2])
+if numbers[3] % 2 == 0:
+    even_numbers.append(numbers[3])
+else:
+    odd_numbers.append(numbers[3])
+if numbers[4] % 2 == 0:
+    even_numbers.append(numbers[4])
+else:
+    odd_numbers.append(numbers[4])
+if numbers[5] % 2 == 0:
+    even_numbers.append(numbers[5])
+else:
+    odd_numbers.append(numbers[5])
 print("Odd numbers:", odd_numbers)
+# [1, 3, 5]
 print("Even numbers:", even_numbers)
-
+# [2, 4, 6]
 
 # 2. Find the Maximum and Minimum Values in a List
 # Write a program to find the maximum and minimum values in a list.
@@ -18,24 +45,53 @@ max_num = numbers[0]
 min_num = numbers[0]
 
 # Compare the rest of the numbers similarly
+if max_num < numbers[1]:
+    max_num = numbers[1]
+if min_num > numbers[1]:
+    min_num = numbers[1]
+if max_num < numbers[2]:
+    max_num = numbers[2]
+if min_num > numbers[2]:
+    min_num = numbers[2]
+if max_num < numbers[3]:
+    max_num = numbers[3]
+if min_num > numbers[3]:
+    min_num = numbers[3]
+if max_num < numbers[4]:
+    max_num = numbers[4]
+if min_num > numbers[4]:
+    min_num = numbers[4]
 
 print("Maximum value:", max_num)
+# 50
 print("Minimum value:", min_num)
-
+# 10
 
 # 3. Find a Specific Value in a List
 # Write a program to find a specific value in a list and print its index. If not found, print "Value not found".
 numbers = [10, 20, 30, 40, 50]
 value = 30
+if value == numbers[0]:
+    print(0)
+elif value == numbers[1]:
+    print(1)
+elif value == numbers[2]:
+    print(2)
+elif value == numbers[3]:
+    print(3)
+elif value == numbers[4]:
+    print(4)
+else:
+    print("Value not found")
 
 
 # 4. Calculate the Sum and Average of a List
 # Write a program to calculate the sum and average of the values in a list.
 # edit total and average
 numbers = [10, 20, 30, 40, 50]
-total = 0
-average = 0
-
+total = numbers[0]+numbers[1]+numbers[2]+numbers[3]+numbers[4]
+average = total / len(numbers)
+print(len("I am Groot"))
 print("Sum:", total)
 print("Average:", average)
 
@@ -44,6 +100,17 @@ print("Average:", average)
 # Write a program to remove duplicate values from a list.
 numbers = [10, 20, 20, 30, 30, 40]
 unique_numbers = []
+if numbers[0] != numbers[1]:
+    unique_numbers.append(numbers[0])
+if numbers[1] != numbers[2]:
+    unique_numbers.append(numbers[1])
+if numbers[2] != numbers[3]:
+    unique_numbers.append(numbers[2])
+if numbers[3] != numbers[4]:
+    unique_numbers.append(numbers[3])
+if numbers[4] != numbers[5]:
+    unique_numbers.append(numbers[4])
+unique_numbers.append(numbers[5])
 
 print("List without duplicates:", unique_numbers)
 
@@ -59,7 +126,7 @@ count_40 = 0
 
 # Compare other numbers similarly
 
-print("Most frequent value and count:")
+# print("Most frequent value and count:")
 # Uncomment below and edit code where (edit here) is
 # if (edit here):
 #     print("30 appears", count_30, "times")
@@ -77,7 +144,7 @@ list1 = [1, 2, 3]
 list2 = [4, 5, 6]
 # edit combined_list
 combined_list = []
-print("Combined list:", combined_list)
+# print("Combined list:", combined_list)
 
 # 8. Check if a List is in Ascending or Descending Order
 # Write a program to check if a list is in ascending or descending order.
@@ -88,4 +155,4 @@ numbers = [10, 20, 30, 40, 50]
 # edit total
 numbers = [1, 2, 3, 4, 5]
 total = 0
-print("Sum of all values:", total)
+# print("Sum of all values:", total)
---
+++ week5/note.py
@@ -0,0 +1,12 @@
+# for (variable) in (iterable)
+#   statement
+num = 100
+my_list = [10, 20, 30, 40, 50]
+# for item in my_list:
+#     print(item)
+# for i in range(len(my_list)):
+#     print(my_list[i])
+for n in range(0, num, 2):
+    print(n)
+
+
