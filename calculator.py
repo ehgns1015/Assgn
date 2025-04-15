4b3206c047a9d96629ea05d6b87da8894e29c825
Ianchoi4253 <wonyoungida4253@gmail.com>
2025-04-15 18:10:13 -0400

    4/15/25

--- calculator.py
+++ calculator.py
@@ -50,18 +50,62 @@ list=[]
 # First ask length,
 # Second input data
 # Last Sum,Average,Max,Min
-length=int(input("write the length: "))
+# length=int(input("write the length: "))
+#
+# for i in range(length):
+#    n=int(input("write the number at " + str(i)+":"))
+#    list.append(n)
+#    print(list)
+# for i in range(length):
+#    list.append(i)
+# print(list)
+# n = input("what is the data at: ")
+# total = sum(list)
+# print("The sum is",sum(list))
+# print ("The average is",sum(list)/len(list))
+# print("The sum is",total)
+# print ("The average is",total/len(list))
+# print("the max is",max(list))
+# print("The min is",min(list))
 
-for i in range(length):
-   list.append(i)
-print(list)
-n = input("what is the data at: ")
-sum = sum(list)
-print("The sum is",sum)
-count = len(list)
-average = sum/count
-print ("The average is",average)
-max = max(list)
-print("the max is",max)
-min = min(list)
-print("The min is",min)
+# x = 1
+# x + 7 =8
+
+# 1 + 7 = 8
+# Create a program that uses a list to
+# store the names of the days of the week,
+# # then asks the user for a number (1-7) and returns the corresponding day.
+# days = ["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"]
+# d = int(input("Pick a number from 1-7:"))
+# #  d should be at least 1 and at most 7
+# if 0 < d < 8:
+#    print(days[d - 1])
+# d = 1
+# d - 1 = 0
+person = {
+   "name":"Ian",
+   "age":12,
+   "city":"Lagrange"
+}
+person1 = dict(name="Ian",age=12,city="Lagrange")
+print(person["name"])
+print(person1.get("name"))
+print(person1.get("grade"))
+person["grade"]= 7
+removed_value = person.pop("city")
+print(removed_value)
+print(person)
+print(person.keys(),person.values(),person.items())
+person1 = person.copy()
+print(person1)
+# Create a simple address book program that allows users to add,
+# view, and delete contacts (name and phone number) using a dictionary
+person2 = {}
+name = input("enter name your name:")
+age = input("enter your age:")
+person2["name"]=name
+person2["age"]=age
+print("Current data:",person2)
+delete = input("Delete 'name' or 'age':")
+person2.pop(delete)
+print("Updated data:", person2)
